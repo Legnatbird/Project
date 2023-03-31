4db47cf132ea63094e701b02c71f0076b9d5226e
Alejandro Quiñones Caicedo <50637445+Legnatbird@users.noreply.github.com>
2023-03-31 12:09:49 -0500

    First Progress

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,20 +1,36 @@
 package model;
 
-import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.io.Console;
 
 public class Controller {
 
 	private Project[] projects;
+	private Console console;
 
 	public Controller() {
-
 		projects = new Project[10];
-	
+	}
+
+	public GregorianCalendar generateDate() {
+		int year = Integer.parseInt(console.readLine("Enter year: "));
+		int month = Integer.parseInt(console.readLine("Enter month: "));
+		int day = Integer.parseInt(console.readLine("Enter day: "));
+
+		return new GregorianCalendar(year, month, day);
 	}
 	
 	//Incomplete
 	public boolean RegisterProject() {
+		if (projects.length < 10) {
+			String name = console.readLine("Enter project name: ");
+			String clientName = console.readLine("Enter client name: ");
+			GregorianCalendar startDate = generateDate();
+			GregorianCalendar endDate = generateDate();
+			double budget = Double.parseDouble(console.readLine("Enter budget: "));
+			projects[projects.length] = new Project(name, clientName, startDate, endDate, budget);
+			return true;
+		}
 
 		return false;
 	}
